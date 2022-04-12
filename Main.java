3950bd2bccd2032f620cf2eaf8b766eb94d75b61
Obvees3 <101640077+obvees@users.noreply.github.com>
2022-04-12 11:49:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+class Main {
+  public static void main(String[] args) {
+    try {
+      Service1 s = new Service1();
+      s.addStudent(new Student("Krzysztof", 20));
+      s.addStudent(new Student("Janusz", 40));
+
+      var students = s.getStudents();
+      for(Student current : students) {
+        System.out.println(current.ToString());
+      }
+    } catch (IOException e) {
+  case 1:
+    System.out.println("imie");
+    imie = scanner.nextLine()
+      System.out.println("wiek");
+    wiek = scanner.nextInt()
+      System.out.println("dodano");
+    method.addStudent(newStudent(imie, wiek));
+    break;
+    case 2:
+    var students = service.getStudents();
+    for(Student current: students)
+      {
+        system.out.println(current.ToString());
+        
+      }
+break;
+    }
+  }
+    }
